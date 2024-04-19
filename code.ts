7254159cc6b809d2eace6f047703b4eac02cd3ff
Samir Hossain <33099952+somir1@users.noreply.github.com>
2024-04-19 15:31:53 -0500

    add more fields

--- code.ts
+++ code.ts
@@ -1,22 +1,17 @@
-figma.showUI(__html__, { width: 300, height: 300 });
-
-async function exportAsPng(node: SceneNode) {
-  const bytes = await node.exportAsync({
-    format: "PNG",
-    constraint: {
-      type: "SCALE",
-      value: 2, // Adjust scaling as needed
-    },
-  });
-  // Send the raw bytes to the UI
-  figma.ui.postMessage({ type: "exported-png", bytes });
-}
+figma.showUI(__html__, { width: 300, height: 400 });
 
 figma.ui.onmessage = async (msg) => {
   if (msg.type === "export-png" && figma.currentPage.selection.length > 0) {
-    await exportAsPng(figma.currentPage.selection[0]);
+    const node = figma.currentPage.selection[0];
+    const bytes = await node.exportAsync({
+      format: "PNG",
+      constraint: { type: "SCALE", value: 2 },
+    });
+    figma.ui.postMessage({
+      type: "exported-png",
+      bytes: Array.from(new Uint8Array(bytes)),
+    });
   } else {
     figma.notify("No component selected");
   }
-  // Don't close the plugin
 };
