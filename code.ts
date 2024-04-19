2300073ff24d9ed6805293e98cb6024b716d0e9d
Samir Hossain <33099952+somir1@users.noreply.github.com>
2024-04-19 18:38:43 -0500

    revert show preview

--- code.ts
+++ code.ts
@@ -1,19 +1,17 @@
 figma.showUI(__html__, { width: 300, height: 400 });
 
 figma.ui.onmessage = async (msg) => {
-  if (msg.type === "export-png") {
-    if (figma.currentPage.selection.length > 0) {
-      const node = figma.currentPage.selection[0];
-      const bytes = await node.exportAsync({
-        format: "JPG", // Changed from PNG to JPG
-        constraint: { type: "SCALE", value: 2 },
-      });
-      figma.ui.postMessage({
-        type: "exported-jpg",
-        bytes: Array.from(new Uint8Array(bytes)),
-      });
-    } else {
-      figma.notify("No component selected");
-    }
+  if (msg.type === "export-png" && figma.currentPage.selection.length > 0) {
+    const node = figma.currentPage.selection[0];
+    const bytes = await node.exportAsync({
+      format: "JPG",
+      constraint: { type: "SCALE", value: 2 },
+    });
+    figma.ui.postMessage({
+      type: "exported-png",
+      bytes: Array.from(new Uint8Array(bytes)),
+    });
+  } else {
+    figma.notify("No component selected");
   }
 };
